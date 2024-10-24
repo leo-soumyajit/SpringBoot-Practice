b17cbd1ed7e6dba8a3ee3891a74d05684dd68877
Soumyajit Banerjee <banerjeesoumyajit2005@gmail.com>
2024-10-24 12:11:45 +0530

    Initial Commit

---
+++ src/test/java/com/Soumyajit/PracticeSpringboot/PracticeSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Soumyajit.PracticeSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
